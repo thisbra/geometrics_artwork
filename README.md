fe683a475316f2270d72124d42caff153ff3b00e
thisbra <thiagosbrasil@ua.pt>
2021-06-10 19:59:32 +0100

    doubleHelix

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # geometrics_artwork
 Playing with graphs on MATLAB doing geodesics and other stuff
 
-1. **Bucky ball**
+1. **Double Helix**
+
+![doubleHelix](doubleHelix.gif)
+
+2. **Bucky ball**
 
 ![buckyball](buckyball.gif)
 
-2. **Hypercube**
+3. **Hypercube**
 
 ![hypercube](hypercube.gif)
--- doubleHelix.m
+++ doubleHelix.m
@@ -1,45 +1,3 @@
-%% Right Helix 1
-
-t = [1:1:80]
-s = [2:1:81]
-
-x = [0:1:80]
-%y = linspace(1,1,81)
-
-
-tt = linspace(-20, 20, length(x))
-
-f = 0.05
-
-yt = sin(f*pi*tt)
-zt = sin(f*pi*tt+pi/2)
-
-
-G = graph(t,s)
-p = plot(G, 'XData', x, 'YData', yt, 'ZData', zt)
-xlabel('x');
-ylabel('y');
-zlabel('z');
-grid()
-
-%% Left Helix 1
-
-hold on
-
-t3 = [1:1:80]
-s3 = [2:1:81]
-
-x3 = [0:1:80]
-%y = linspace(1,1,81)
-
-tt3 = linspace(-20, 20, length(x3))
-
-yt3 = sin(f*pi*tt3+pi)
-zt3 = sin(f*pi*tt3+1.5*pi)
-
-G3 = graph(t3,s3)
-plot(G3, 'XData', x3, 'YData', yt3, 'ZData', zt3)
-
 %% Merge try
 t = [1:1:160]
 s = [2:1:161]
@@ -61,42 +19,16 @@ zt(1:81) = sin(f*pi*tt+pi/2)
 zt(81:161) = sin(f*pi*tt+1.5*pi)
 
 G = graph(t,s)
-plot(G, 'XData', x, 'YData', yt, 'ZData', zt)
-%%
-f = 0.05
 
-yt = sin(f*pi*tt)
-zt = sin(f*pi*tt+pi/2)
+p = plot(G, 'XData', x, 'YData', yt, 'ZData', zt, 'LineWidth', 10)
+p.NodeColor = 'r'
+p.MarkerSize = 10;
+set(gca, 'Color', 'k')
 
-
-G = graph(t,s)
-p = plot(G, 'XData', x, 'YData', yt, 'ZData', zt)
-
-%% Right Helix 2
-
-t2 = [1:1:80]
-s2 = [2:1:81]
-
-x2 = [81:1:161]
-%y = linspace(1,1,81)
-
-
-tt2 = linspace(-20, 20, length(x))
-
-
-yt2 = sin(f*pi*tt2)
-zt2 = sin(f*pi*tt2+pi/2)
-
-G2 = graph(t2,s2)
-plot(G2, 'XData', x2, 'YData', yt2, 'ZData', zt2)
-
-
-
-%% Left Helix 2
-
-
-
-
-%% ATCG's
-
-
+for v=1:120
+    
+    view(0, v);
+    v = v + 1
+    pause(0.1);
+end
+p
